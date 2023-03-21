ebe25bcc6b1de446ecbf8fbced71802722dcb250
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-03-21 16:42:20 +0100

    dynamic refactor

--- 05-menu/05-menu/src/Categories.js
+++ 05-menu/05-menu/src/Categories.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ filterItems, items }) => {
   return (
     <div className="btn-container">
-      <button className="filter-btn" onClick={() => filterItems('')}>
+      {items.map((category, idx) => {
+        return (
+          <button
+            key={idx}
+            type="button"
+            className="filter-btn"
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+      {/* <button className="filter-btn" onClick={() => filterItems('')}>
         All
       </button>
       <button className="filter-btn" onClick={() => filterItems('breakfast')}>
@@ -14,7 +26,7 @@ const Categories = ({ filterItems }) => {
       </button>
       <button className="filter-btn" onClick={() => filterItems('shakes')}>
         Shakes
-      </button>
+      </button> */}
     </div>
   );
 };
