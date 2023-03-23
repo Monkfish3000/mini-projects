5abd09e0108598102a70f01a7cf9bdd2b2d73e0b
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-03-23 16:55:11 +0100

    display jobs

--- 06-tabs/06-tabs/src/App.js
+++ 06-tabs/06-tabs/src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const fetchJobs = async () => {
     const response = await fetch(url);
-    const newJobs = response.json();
+    const newJobs = await response.json();
 
     setJobs(newJobs);
     setLoading(false);
@@ -27,8 +27,32 @@ function App() {
       </section>
     );
   }
-
-  return <h2>Jobs</h2>;
+  const { company, dates, duties, title } = jobs[value];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-centre">
+        {/* {btn container} */}
+        {/* {job info} */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p className="duty">{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default App;
