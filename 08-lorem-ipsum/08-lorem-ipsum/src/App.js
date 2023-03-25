379968e748cb49a8d2ba4abbedbec0ec4a7d2e2b
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-03-25 10:49:53 +0100

    functionality added to lorem generator

--- 08-lorem-ipsum/08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/08-lorem-ipsum/src/App.js
@@ -6,6 +6,10 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    let amount = parseInt(count);
+    if (count > 8) amount = 8;
+
+    amount <= 0 ? setText(data.slice(0, 1)) : setText(data.slice(0, amount));
   };
   return (
     <section className="section-center">
@@ -24,12 +28,9 @@ function App() {
         </button>
       </form>
       <article className="lorem-text">
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, quos.
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, quos.
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
