8e10147019ad96bea6250cd0cb73b789175b6d97
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-03-25 10:34:55 +0100

    lorem form setup

---
+++ 08-lorem-ipsum/08-lorem-ipsum/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import data from './data';
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amout">PARAGRAPHS:</label>
+        <input
+          type="number"
+          name="name"
+          id="amout"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, quos.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, quos.
+        </p>
+      </article>
+    </section>
+  );
+}
+
+export default App;
