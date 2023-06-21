4485f00d02ac23c91ec14e2fbfd6c8bab6763095
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-06-21 16:08:06 +0200

    css fix

--- 10-grocery-bud/10-grocery-bud/src/SingleItem.jsx
+++ 10-grocery-bud/10-grocery-bud/src/SingleItem.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SingleItem = () => {
-  return <h2>SingleItem</h2>;
+const SingleItem = ({ item, removeItem }) => {
+  const [isChecked, setIsChecked] = useState(item.completed);
+
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p
+        style={{
+          textTransform: 'capitalize',
+          textDecoration: isChecked && 'line-through',
+        }}
+      >
+        {item.name}
+      </p>
+      <button type="button" className="btn remove-btn">
+        delete
+      </button>
+    </div>
+  );
 };
 
 export default SingleItem;
